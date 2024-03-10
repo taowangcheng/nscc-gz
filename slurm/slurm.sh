0c6af4b6ae9cae7b8f246520131f047a3b3b52b0
dangmai <dangmai@hust.edu.cn>
2024-03-10 23:48:41 +0800

    fix slurm scripts

--- slurm/slurm.sh
+++ slurm/slurm.sh
@@ -2,4 +2,11 @@
 
 module load anaconda3/2023.09
 source activate pytorch2
-torchrun --nnodes=2 --nproc-per-node=8 --rdzv-id=1234 --rdzv-backend=c10d --rdzv-endpoint=$1:1234 slurm.py
+# torchrun --nnodes=2 --nproc-per-node=8 --rdzv-id=1234 --rdzv-backend=c10d --rdzv-endpoint=$1:1234 slurm.py
+run_cmd="torchrun --nnodes=2 --nproc-per-node=8 --rdzv-id=1234 --rdzv-backend=c10d --rdzv-endpoint=$1:1234 slurm.py"
+
+echo $run_cmd
+# sleep 100000
+eval $run_cmd
+
+set +x
